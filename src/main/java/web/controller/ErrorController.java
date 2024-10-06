d67b994c1de4faafdeb0848fd78e876250b0e42e
lombrozomir <roman.ryzhkov31@gmail.com>
2024-10-06 10:37:07 +0300

    Update controller.

--- src/main/java/web/controller/ErrorController.java
+++ src/main/java/web/controller/ErrorController.java
@@ -5,11 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorController {
-
     @GetMapping("/error")
     public String errorPage() {
         return "error";
     }
-
-
 }
